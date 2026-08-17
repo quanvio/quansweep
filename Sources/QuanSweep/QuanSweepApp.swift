acd6e98885787dc87b86730a4b5b14e0d6ae0926
Debaranjan Pegu <devianpegu@gmail.com>
2026-08-17 15:09:18 +0530

    Fix crash on launch: inject AppViewModel into environment

--- Sources/QuanSweep/QuanSweepApp.swift
+++ Sources/QuanSweep/QuanSweepApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct QuanSweepApp: App {
+    @StateObject private var viewModel = AppViewModel()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(viewModel)
                 .frame(minWidth: 900, minHeight: 650)
         }
         .windowResizability(.contentSize)
